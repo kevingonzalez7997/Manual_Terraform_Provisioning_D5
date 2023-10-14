a719ef8347d8c37312db7bfa42e0b8283b2f4944
kevingonzalez7997 <59447523+kevingonzalez7997@users.noreply.github.com>
2023-10-13 21:52:02 -0400

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 rm -rf c4_deployment-5
 source test/bin/activate
-git clone https://github.com/kura-labs-org/c4_deployment-5.git
-cd c4_deployment-5
+git clone https://github.com/kevingonzalez7997/Terraform_D5.git
+cd Terraform_D5
 pip install -r requirements.txt
 pip install gunicorn
 python database.py
